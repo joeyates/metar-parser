90b2ab7f048214b8690f4350aa844bad882235b1
Joe Yates <joe.g.yates@gmail.com>
2010-02-06 18:51:19 +0100

    Removed parser related code.

--- lib/metar.rb
+++ lib/metar.rb
@@ -1,8 +1,5 @@
 require 'rubygems' if RUBY_VERSION < '1.9'
 require 'dhaka'
 require File.join(File.dirname(__FILE__), 'metar', 'raw')
-require File.join(File.dirname(__FILE__), 'metar', 'lexer')
-require File.join(File.dirname(__FILE__), 'metar', 'parser')
-require File.join(File.dirname(__FILE__), 'metar', 'evaluator')
 require File.join(File.dirname(__FILE__), 'metar', 'station')
 require File.join(File.dirname(__FILE__), 'metar', 'report')
--- lib/metar/lexer.rb
+++
@@ -1,9 +0,0 @@
-require 'rubygems' if RUBY_VERSION < '1.9'
-require 'dhaka'
-# require 'dhaka/runtime' switch to this when the grammar has stablized
-require File.dirname(__FILE__) + '/lexer_specification'
-
-if not defined?(Metar::Lexer)
-  lexer = Dhaka::Lexer.new(Metar::LexerSpecification)
-  eval(lexer.compile_to_ruby_source_as('Metar::Lexer'.intern))
-end
