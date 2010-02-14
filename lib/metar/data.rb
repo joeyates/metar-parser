a16229315e4a782a5d9b3d96b226e514d0957fcb
Joe Yates <joe.g.yates@gmail.com>
2010-02-14 19:16:00 +0100

    Separated data container classes from report.

---
+++ lib/metar/data.rb
@@ -0,0 +1,226 @@
+module Metar
+
+  class Temperature
+    def initialize(s)
+      @unit = :celcius
+      if s =~ /^(M?)(\d+)$/
+        sign = $1
+        value = $2
+        if value != 'XX'
+          @value = value.to_i
+          @value *= -1 if sign == 'M'
+        end
+      else
+        @value = nil
+      end
+    end
+
+    def to_s
+      @value ? "#{ @value } celcius" : 'Not available'
+    end
+  end
+
+  class Speed
+    UNITS = {
+      ''    => :kilometers_per_hour,
+      'KMH' => :kilometers_per_hour,
+      'KT'  => :knots,
+      'MPS' => :meters_per_second      
+    }
+    def Speed.parse(s)
+      if s =~ /^(\d+)(KT|MPS|KMH|)/
+        new($1.to_i, UNITS[$2])
+      else
+      end
+    end
+
+    def initialize(value, unit = :kilometers_per_hour)
+      @value, @unit = value, unit
+    end
+
+    def to_s
+      "#{ @value } #{ @unit }"
+    end
+  end
+
+  class Visibility
+    def Visibility.parse(s)
+      case
+      when s == '9999'
+        new('More than 10 km')
+      when s =~ /(\d{4})NDV/ # WMO, TODO NDV
+        new(Distance.new($1))
+      when (s =~ /^((1|2)\s|)([13])\/([24])SM$/) # US
+        miles = $1.to_f + $3.to_f / $4.to_f
+        new(Distance.new(miles, :miles))
+      when s =~ /^(\d+)KM$/
+        new(Distance.new($1))
+      when s =~ /^(\d+)(N|NE|E|SE|S|SW|W|NW)?$/ # TODO direction
+        new(Distance.new($1))
+      when s == 'M1/4SM' # US
+        new('Less than 1/4 mile')
+      when s =~ /^(\d+)SM$/ # US
+        new(Distance.new($1, :miles))
+      else
+        nil
+      end
+    end
+
+    def initialize(visibility, direction = nil) # visibilty can be String, Distance
+      @visibility = visibility
+    end
+
+    def to_s
+      @visibility.to_s
+    end
+  end
+
+  class Distance
+    def initialize(value, unit = :meters)
+      @value, @unit = value, unit
+    end
+
+    def to_s
+      "#{ @value } #{ @unit }"
+    end
+  end
+
+  class Wind
+    def Wind.recognize(s)
+      case
+      when s =~ /^(\d{3})(\d{2}(KT|MPS|KMH|))$/
+        new($1, Speed.parse($2))
+      when s =~ /^(\d{3})(\d{2})G(\d{2,3}(KT|MPS|KMH|))$/
+        new($1, Speed.parse($2)) # TODO
+      when s =~ /^VRB(\d{2}(KT|MPS|KMH|))$/
+        new('variable direction', Speed.parse($2))
+      when s =~ /^\/{3}(\d{2}(KT|MPS|KMH|))$/
+        new('unknown direction', Speed.parse($2))
+      when s =~ /^\/{3}(\/{3}(KT|MPS|KMH|))$/
+        new('unknown direction', 'unknown')
+      else
+        nil
+      end
+    end
+
+    def initialize(direction, speed)
+      @direction, @speed = direction, speed
+    end
+
+    def to_s
+      "#{ @direction } degrees #{ @speed }"
+    end
+  end
+
+  class WeatherPhenomenon
+    Phenomena = {
+      'BR' => 'mist',
+      'DU' => 'dust',
+      'DZ' => 'drizzle',
+      'FG' => 'fog',
+      'FU' => 'smoke',
+      'GR' => 'hail',
+      'GS' => 'small hail',
+      'HZ' => 'haze',
+      'IC' => 'ice crystals',
+      'PL' => 'ice pellets',
+      'PO' => 'dust whirls',
+      'PY' => '???', # TODO
+      'RA' => 'rain',
+      'SA' => 'sand',
+      'SN' => 'snow',
+      'SG' => 'snow grains',
+      'SNRA' => 'snow and rain',
+      'SQ' => 'squall',
+      'UP' => 'unknown phenomenon',
+      'VA' => 'volcanic ash',
+      'FC' => 'funnel cloud',
+      'SS' => 'sand storm',
+      'DS' => 'dust strom',
+    }
+
+    Descriptors = {
+      'BC' => 'patches of ',
+      'BL' => 'blowing ',
+      'DR' => 'low drifting ',
+      'FZ' => 'freezing ',
+      'MI' => 'shallow ',
+      'PR' => 'partial ',
+      'SH' => 'shower of ',
+      'TS' => 'thunderstorm and ',
+    }
+
+    Modifiers = {
+      '\+' => 'heavy ',
+      '-' => 'light ',
+      'VC' => 'nearby '
+    }
+
+    def WeatherPhenomenon.recognize(s)
+      codes = Phenomena.keys.join('|')
+      descriptors = Descriptors.keys.join('|')
+      modifiers = Modifiers.keys.join('|')
+      rxp = Regexp.new("^(#{ modifiers })?(#{ descriptors })?(#{ codes })$")
+      if rxp.match(s)
+        modifier_code = $1
+        descriptor_code = $2
+        phenomenon_code = $3
+        Metar::WeatherPhenomenon.new(Phenomena[phenomenon_code], Modifiers[modifier_code], Descriptors[descriptor_code])
+      else
+        nil
+      end
+    end
+
+    def initialize(phenomenon, modifier = nil, descriptor = nil)
+      @phenomenon, @modifier, @descriptor = phenomenon, modifier, descriptor
+    end
+
+    def to_s
+      "#{ @modifier }#{ @descriptor }#{ @phenomenon }"
+    end
+  end
+
+  class SkyCondition
+    def SkyCondition.recognize(s)
+      case
+      when s == 'NSC' # WMO
+        'No significant cloud'
+      when s == 'CLR' # TODO - meaning?
+        'Clear skies'
+      when s == 'SKC' # TODO - meaning?
+        'Clear skies'
+      when s =~ /^(BKN|FEW|OVC|SCT)(\d+)(CB|TCU|\/{3})?$/
+        height = $2.to_i * 30
+        type = case $3
+               when nil
+                 ''
+               when 'CB'
+                 'cumulonimbus '
+               when 'TCU'
+                 'towering cumulus '
+               when '///'
+                 ''
+               end
+        case $1
+        when 'BKN'
+          "Broken #{ type }cloud at #{ height }"
+        when 'FEW'
+          "Few #{ type }clouds at #{ height }"
+        when 'OVC'
+          "Overcast #{ type }at #{ height }"
+        when 'SCT'
+          "Scattered #{ type }cloud at #{ height }"
+        end
+      when s =~ /^VV(\d{3}|\/\/\/)?$/
+        height = case $1
+                 when '///'
+                   'unknown'
+                 else
+                   $1.to_i
+                 end
+        "Vertical visibility #{ height }"
+      end
+    end
+  end
+
+end
--- lib/metar/report.rb
+++ lib/metar/report.rb
@@ -1,235 +1,11 @@
 require 'rubygems' if RUBY_VERSION < '1.9'
 require 'aasm'
+require File.join(File.dirname(__FILE__), 'data')
 
 module Metar
-  STANDARD_WMO       = :wmo
-  STANDARD_US        = :us
   OBSERVER_REAL      = :real
   OBSERVER_CORRECTED = :corrected
   OBSERVER_AUTO      = :auto
-
-  class Temperature
-    def initialize(s)
-      @unit = :celcius
-      if s =~ /^(M?)(\d+)$/
-        sign = $1
-        value = $2
-        if value != 'XX'
-          @value = value.to_i
-          @value *= -1 if sign == 'M'
-        end
-      else
-        @value = nil
-      end
-    end
-
-    def to_s
-      @value ? "#{ @value } celcius" : 'n.a.'
-    end
-  end
-
-  class Speed
-    UNITS = {
-      ''    => :kilometers_per_hour,
-      'KMH' => :kilometers_per_hour,
-      'KT'  => :knots,
-      'MPS' => :meters_per_second      
-    }
-    def Speed.parse(s)
-      if s =~ /^(\d+)(KT|MPS|KMH|)/
-        new($1.to_i, UNITS[$2])
-      else
-      end
-    end
-
-    def initialize(value, unit = :kilometers_per_hour)
-      @value, @unit = value, unit
-    end
-
-    def to_s
-      "#{ @value } #{ @unit }"
-    end
-  end
-
-  class Visibility
-    def Visibility.parse(s)
-      case
-      when s == '9999'
-        new('More than 10 km')
-      when s =~ /(\d{4})NDV/ # WMO, TODO NDV
-        new(Distance.new($1))
-      when (s =~ /^((1|2)\s|)([13])\/([24])SM$/) # US
-        miles = $1.to_f + $3.to_f / $4.to_f
-        new(Distance.new(miles, :miles))
-      when s =~ /^(\d+)KM$/
-        new(Distance.new($1))
-      when s =~ /^(\d+)(N|NE|E|SE|S|SW|W|NW)?$/ # TODO direction
-        new(Distance.new($1))
-      when s == 'M1/4SM' # US
-        new('Less than 1/4 mile')
-      when s =~ /^(\d+)SM$/ # US
-        new(Distance.new($1, :miles))
-      else
-        nil
-      end
-    end
-
-    def initialize(visibility, direction = nil) # visibilty can be String, Distance
-      @visibility = visibility
-    end
-
-    def to_s
-      @visibility.to_s
-    end
-  end
-
-  class Distance
-    def initialize(value, unit = :meters)
-      @value, @unit = value, unit
-    end
-
-    def to_s
-      "#{ @value } #{ @unit }"
-    end
-  end
-
-  class Wind
-    def Wind.recognize(s)
-      case
-      when s =~ /^(\d{3})(\d{2}(KT|MPS|KMH|))$/
-        new($1, Speed.parse($2))
-      when s =~ /^(\d{3})(\d{2})G(\d{2,3}(KT|MPS|KMH|))$/
-        new($1, Speed.parse($2)) # TODO
-      when s =~ /^VRB(\d{2}(KT|MPS|KMH|))$/
-        new('variable direction', Speed.parse($2))
-      when s =~ /^\/{3}(\d{2}(KT|MPS|KMH|))$/
-        new('unknown direction', Speed.parse($2))
-      when s =~ /^\/{3}(\/{3}(KT|MPS|KMH|))$/
-        new('unknown direction', 'unknown')
-      else
-        nil
-      end
-    end
-
-    def initialize(direction, speed)
-      @direction, @speed = direction, speed
-    end
-
-    def to_s
-      "#{ @direction } degrees #{ @speed }"
-    end
-  end
-
-  class WeatherPhenomenon
-    Phenomena = {
-      'BR' => 'mist',
-      'DU' => 'dust',
-      'DZ' => 'drizzle',
-      'FG' => 'fog',
-      'FU' => 'smoke',
-      'GR' => 'hail',
-      'GS' => 'small hail',
-      'HZ' => 'haze',
-      'IC' => 'ice crystals',
-      'PL' => 'ice pellets',
-      'PO' => 'dust whirls',
-      'PY' => '???', # TODO
-      'RA' => 'rain',
-      'SA' => 'sand',
-      'SN' => 'snow',
-      'SG' => 'snow grains',
-      'SNRA' => 'snow and rain',
-      'SQ' => 'squall',
-      'UP' => 'unknown phenomenon',
-      'VA' => 'volcanic ash',
-      'FC' => 'funnel cloud',
-      'SS' => 'sand storm',
-      'DS' => 'dust strom',
-    }
-
-    Descriptors = {
-      'BC' => 'patches of ',
-      'BL' => 'blowing ',
-      'DR' => 'low drifting ',
-      'FZ' => 'freezing ',
-      'MI' => 'shallow ',
-      'PR' => 'partial ',
-      'SH' => 'shower of ',
-      'TS' => 'thunderstorm and ',
-    }
-
-    Modifiers = {
-      '\+' => 'heavy ',
-      '-' => 'light ',
-      'VC' => 'nearby '
-    }
-
-    def WeatherPhenomenon.recognize(s)
-      codes = Phenomena.keys.join('|')
-      descriptors = Descriptors.keys.join('|')
-      modifiers = Modifiers.keys.join('|')
-      rxp = Regexp.new("^(#{ modifiers })?(#{ descriptors })?(#{ codes })$")
-      if rxp.match(s)
-        modifier_code = $1
-        descriptor_code = $2
-        phenomenon_code = $3
-        Metar::WeatherPhenomenon.new(Phenomena[phenomenon_code], Modifiers[modifier_code], Descriptors[descriptor_code])
-      else
-        nil
-      end
-    end
-
-    def initialize(phenomenon, modifier = nil, descriptor = nil)
-      @phenomenon, @modifier, @descriptor = phenomenon, modifier, descriptor
-    end
-
-    def to_s
-      "#{ @modifier }#{ @descriptor }#{ @phenomenon }"
-    end
-  end
-
-  class SkyCondition
-    def SkyCondition.recognize(s)
-      case
-      when s == 'NSC' # WMO
-        'No significant cloud'
-      when s == 'CLR' # TODO - meaning?
-        'Clear skies'
-      when s == 'SKC' # TODO - meaning?
-        'Clear skies'
-      when s =~ /^(BKN|FEW|OVC|SCT)(\d+)(CB|TCU|\/{3})?$/
-        height = $2.to_i * 30
-        type = case $3
-               when nil
-                 ''
-               when 'CB'
-                 'cumulonimbus '
-               when 'TCU'
-                 'towering cumulus '
-               when '///'
-                 ''
-               end
-        case $1
-        when 'BKN'
-          "Broken #{ type }cloud at #{ height }"
-        when 'FEW'
-          "Few #{ type }clouds at #{ height }"
-        when 'OVC'
-          "Overcast #{ type }at #{ height }"
-        when 'SCT'
-          "Scattered #{ type }cloud at #{ height }"
-        end
-      when s =~ /^VV(\d{3}|\/\/\/)?$/
-        height = case $1
-                 when '///'
-                   'unknown'
-                 else
-                   $1.to_i
-                 end
-        "Vertical visibility #{ height }"
-      end
-    end
-  end
 
   class Report
     include AASM
@@ -420,22 +196,23 @@ module Metar
     def seek_visibility
       if @chunks[0] == 'CAVOK'
         @chunks.shift
-        @visibility = 'More than 10km'
+        @visibility = Visibility.new('More than 10km')
         @present_weather << Metar::WeatherPhenomenon.new('No significant weather')
-        @sky_conditions << 'No significant cloud' # TODO. What does NSC stand for?
+        @sky_conditions << 'No significant cloud' # TODO: What does NSC stand for?
         cavok!
         return
       end
 
       if @observer == OBSERVER_AUTO # WMO 15.4
         if @chunks[0] == '////'
-          @visibility = @chunks.shift # TODO - should be 'not observed'
+          @chunks.shift
+          @visibility = Visibility.new('Not observed')
           visibility!
           return
         end
       end
 
-      if @chunks[0] == '1' and @chunks[0] == '2'
+      if @chunks[0] == '1' or @chunks[0] == '2'
         visibility = Visibility.parse(@chunks[0] + ' ' + @chunks[1])
         if visibility
           @chunks.shift
@@ -502,7 +279,8 @@ module Metar
     def seek_sky_conditions
       if @observer == OBSERVER_AUTO # WMO 15.4
         if @chunks[0] == '///' or @chunks[0] == '//////'
-          @sky_conditions << @chunks.shift # TODO - should be 'not observed'
+          @chunks.shift
+          @sky_conditions << 'Not observed'
           sky_conditions!
           return
         end
