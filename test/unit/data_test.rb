83b26458428d93add8c815a2002424407609ec00
Joe Yates <joe.g.yates@gmail.com>
2010-03-09 17:01:38 +0100

    All data tests now passing again.

--- test/unit/data_test.rb
+++ test/unit/data_test.rb
@@ -21,18 +21,18 @@ class TestMetarData < Test::Unit::TestCase
 
   def test_temperature_parse_positive
     temperature = Metar::Temperature.parse('12')
-    assert(temperature.value == 12)
+    assert_equal(12, temperature.value)
   end
 
   def test_temperature_parse_negative
     temperature = Metar::Temperature.parse('M12')
-    assert(temperature.value == -12)
+    assert_equal(-12, temperature.value)
   end
 
   # Direction
   def test_direction_handles_leading_zero
     direction = Metar::Direction.new('010')
-    assert(direction.to_s == '10°')
+    assert_equal('10°', direction.to_s)
   end
 
   # Speed
@@ -43,24 +43,24 @@ class TestMetarData < Test::Unit::TestCase
 
   def test_speed_parse_default_unit
     speed = Metar::Speed.parse('12')
-    assert(speed.value == 12)
-    assert(speed.unit == :kilometers_per_hour)
+    assert_equal(12, speed.value)
+    assert_equal(:kilometers_per_hour, speed.unit)
   end
 
   def test_speed_parse_kilometers_per_hour
     speed = Metar::Speed.parse('12KMH')
-    assert(speed.value == 12)
-    assert(speed.unit == :kilometers_per_hour)
+    assert_equal(12, speed.value)
+    assert_equal(:kilometers_per_hour, speed.unit)
   end
 
   def test_speed_parse_knots
     speed = Metar::Speed.parse('12KT')
-    assert(speed.unit == :knots)
+    assert(:knots, speed.unit)
   end
 
   def test_speed_parse_meters_per_second
     speed = Metar::Speed.parse('12MPS')
-    assert(speed.unit == :meters_per_second)
+    assert_equal(:meters_per_second, speed.unit)
   end
 
   # Visibility
@@ -76,41 +76,42 @@ class TestMetarData < Test::Unit::TestCase
 
   def test_visibility_parse_9999
     visibility = Metar::Visibility.parse('9999')
-    assert(visibility.to_s == 'more than 10 kilometers')
+    assert_equal('more than 10 kilometers', visibility.to_s)
   end
 
   def test_visibility_parse_ndv
     visibility = Metar::Visibility.parse('0200NDV')
-    assert(visibility.distance.value == 200)
+    assert_equal(200, visibility.distance.value)
     assert_nil(visibility.direction)
   end
 
   def test_visibility_parse_us_fractions_1_4
     visibility = Metar::Visibility.parse('1/4SM')
-    assert(visibility.distance.value == 0.25)
-    assert(visibility.distance.unit == :miles)
+    assert_equal(Metar::Distance.miles(0.25), visibility.distance.value)
+    assert_equal(:miles, visibility.distance.options[:units])
   end
 
   def test_visibility_parse_us_fractions_2_1_2
     visibility = Metar::Visibility.parse('2 1/2SM')
-    assert(visibility.distance.value == 2.5)
-    assert(visibility.distance.unit == :miles)
+    assert_equal(Metar::Distance.miles(2.5), visibility.distance.value)
+    assert_equal(:miles, visibility.distance.options[:units])
   end
 
   def test_visibility_parse_kilometers
     visibility = Metar::Visibility.parse('5KM')
-    assert(visibility.distance.value == 5.0)
-    assert(visibility.distance.unit == :kilometers)
+    assert_equal(5000.0, visibility.distance.value)
+    assert_equal(:kilometers, visibility.distance.options[:units])
   end
 
   def test_visibility_parse_compass
     visibility = Metar::Visibility.parse('5NE')
-    assert(visibility.distance.value == 5.0)
-    assert(visibility.distance.unit == :kilometers)
-    assert(visibility.direction.value == 45)
-    Metar::Distance.output.unit = :metric
-    Metar::Distance.output.style = :abbreviated
-    Metar::Direction.output.unit = :compass
-    assert(visibility.to_s == '5km NE')
+    assert_equal(5000.0, visibility.distance.value)
+    assert_equal(:kilometers, visibility.distance.options[:units])
+    assert_equal(45.0, visibility.direction.value)
+    visibility.distance.options[:units] = :kilometers
+    visibility.distance.options[:abbreviated] = true
+    visibility.distance.options[:decimals] = 0
+    visibility.direction.options[:units] = :compass
+    assert_equal('5km NE', visibility.to_s)
   end
 end
