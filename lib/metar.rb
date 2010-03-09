0b41286a637e566e420b011dbbf11648d5003e15
Joe Yates <joe.g.yates@gmail.com>
2010-03-09 12:51:31 +0100

    Getting ready for initial release. Added a version and a Rakefile which builds the gem.

--- lib/metar.rb
+++ lib/metar.rb
@@ -2,3 +2,15 @@ require File.join(File.dirname(__FILE__), 'metar', 'raw')
 require File.join(File.dirname(__FILE__), 'metar', 'station')
 require File.join(File.dirname(__FILE__), 'metar', 'parser')
 require File.join(File.dirname(__FILE__), 'metar', 'report')
+
+module Metar
+
+  module VERSION #:nodoc:
+    MAJOR = 0
+    MINOR = 1
+    TINY = 1
+ 
+    STRING = [MAJOR, MINOR, TINY].join('.')
+  end
+
+end
