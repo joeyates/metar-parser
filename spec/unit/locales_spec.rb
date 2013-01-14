acdb551f04f892c1401c5475c114bf782425fcf7
Joe Yates <joe.g.yates@gmail.com>
2013-01-14 21:37:31 +0100

    Add expectation for locale loading

---
+++ spec/unit/locales_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe 'locales' do
+  it 'adds our locales to the load path' do
+    project_root = File.expand_path('../..', File.dirname(__FILE__))
+    english_path = File.join(project_root, 'locales', 'en.yml')
+    italian_path = File.join(project_root, 'locales', 'it.yml')
+    german_path  = File.join(project_root, 'locales', 'de.yml')
+
+    expect(I18n.load_path).to include(english_path)
+    expect(I18n.load_path).to include(italian_path)
+    expect(I18n.load_path).to include(german_path)
+  end
+end
+
