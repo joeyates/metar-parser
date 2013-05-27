6326d26eed84034bcb332c5e090723bfbf165ca9
Joe Yates <joe.g.yates@gmail.com>
2013-05-27 05:21:33 +0200

    Whitespace

--- spec/unit/sky_condition_spec.rb
+++ spec/unit/sky_condition_spec.rb
@@ -1,8 +1,8 @@
 # encoding: utf-8
 load File.expand_path( '../spec_helper.rb', File.dirname(__FILE__) )
 
-RSpec::Matchers.define :be_sky_condition do | quantity, height, type |
-  match do | sk |
+RSpec::Matchers.define :be_sky_condition do |quantity, height, type|
+  match do |sk|
     if    sk.nil? && quantity == :expect_nil
       true
     elsif sk.nil? && quantity != :expect_nil
@@ -20,23 +20,20 @@ RSpec::Matchers.define :be_sky_condition do | quantity, height, type |
 end
 
 describe Metar::SkyCondition do
-
   context '.parse' do
-
     [
-      [ 'understands clear skies codes', 'NSC',      [ nil,            nil,            nil ] ],
-      [ 'quantity + height',             'BKN12',    [ 'broken',    365.76,            nil ] ],
-      [ 'quantity + height + type',      'BKN12CB',  [ 'broken',    365.76, 'cumulonimbus' ] ],
-      [ 'quantity + ///',                'BKN///',   [ 'broken',       nil,            nil ] ],
-      [ 'quantity + height + ///',       'FEW038///',[ 'few',      1158.24,            nil ] ],
-      [ 'cumulonimbus only',             'CB',       [ nil,            nil, 'cumulonimbus' ] ], # seems non-standard, but occurs
-      [ 'returns nil for unmatched',     'FUBAR',    [ :expect_nil,    nil,            nil ] ],
-    ].each do | docstring, raw, expected |
+      ['understands clear skies codes', 'NSC',      [nil,            nil,            nil]],
+      ['quantity + height',             'BKN12',    ['broken',    365.76,            nil]],
+      ['quantity + height + type',      'BKN12CB',  ['broken',    365.76, 'cumulonimbus']],
+      ['quantity + ///',                'BKN///',   ['broken',       nil,            nil]],
+      ['quantity + height + ///',       'FEW038///',['few',      1158.24,            nil]],
+      ['cumulonimbus only',             'CB',       [nil,            nil, 'cumulonimbus']], # seems non-standard, but occurs
+      ['returns nil for unmatched',     'FUBAR',    [:expect_nil,    nil,            nil]],
+    ].each do |docstring, raw, expected|
       example docstring do
-        Metar::SkyCondition.parse( raw ).should be_sky_condition( *expected )
+        Metar::SkyCondition.parse(raw).should be_sky_condition(*expected)
       end
     end
-
   end
 
   context '.to_summary' do
@@ -54,20 +51,17 @@ describe Metar::SkyCondition do
   end
 
   context '.to_s' do
-
     [
-      [ 'all values nil',  [ nil,      nil, nil ],           'clear skies'                ],
-      [ 'quantity',        [ 'broken', 360, nil ],           'broken cloud at 360'        ],
-      [ 'quantity + type', [ 'broken', 360, 'cumulonimbus'], 'broken cumulonimbus at 360' ],
-    ].each do | docstring, ( quantity, height, type ), expected |
+      ['all values nil',  [nil,      nil, nil],            'clear skies'               ],
+      ['quantity',        ['broken', 360, nil],            'broken cloud at 360'       ],
+      ['quantity + type', ['broken', 360, 'cumulonimbus'], 'broken cumulonimbus at 360'],
+    ].each do |docstring, (quantity, height, type), expected|
       example docstring do
         sk = Metar::SkyCondition.new( quantity, height, type )
       
         sk.to_s.                  should     == expected
       end
     end
-
   end
-
 end
 
